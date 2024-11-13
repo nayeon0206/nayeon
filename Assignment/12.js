8146f238e05456ec46d9a39fec5ed772f6844757
강나연 <skdusdl4@naver.com>
2024-11-13 09:48:27 +0900

    Update 12.js

--- Assignment/12.js
+++ Assignment/12.js
@@ -1,10 +1,17 @@
 // 평균 구하기
 
+// 배열의 평균 구하는 것과 같음
+
 function solution(numbers) {
     let sum = 0;
-    for (let i =0; i < numbers; i++) {
+    for (let i =0; i < numbers.length; i++) {
         sum += numbers[i]
     }
-    let answer = sum / numbers;
+    let answer = sum / numbers.length;
+    return answer;
+}
+
+function solution(numbers) {
+    let answer = numbers.reduce((a,b)=> a+b,0)/numbers.length
     return answer;
 }
