954e89d8dbbb2bb492d9cd9733662e5ed8b47328
강나연 <skdusdl4@naver.com>
2024-11-15 09:58:17 +0900

    Update 11.js

---
+++ sparta/prac/11.js
@@ -0,0 +1,24 @@
+// pop() {
+//     if (this.head === null) {
+//         return null; 
+//     }
+//     let deleteHead = this.head;
+//     this.head = this.head.next;
+//     return deleteHead;
+// }
+
+// function selectionSort (array) {
+//     let n = array.length
+//     for (let i = 0; i < n-1; i++) {
+//         let minIndex = i;
+//         for (let j = i + 1; j <n; j++) {
+//             if (array[j] < array[minIndex] ) {
+//                 minIndex = j;
+//             }
+//         }
+//         let temp = array[i];
+//     array[i] = array[minIndex];
+//     array[minIndex] = temp;
+//     }
+//     return array;
+// }
