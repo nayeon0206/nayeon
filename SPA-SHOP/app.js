3fb34aabcafe0e9e8665410b2616fb2dc07c07f0
강나연 <skdusdl4@naver.com>
2024-11-21 20:31:36 +0900

    강의 1주차 마지막

--- SPA-SHOP/app.js
+++ SPA-SHOP/app.js
@@ -7,6 +7,14 @@ import newsRouter from './routes/news.js';
 const app = express();
 const PORT = 3000; // 서버를 열 때 사용할 포트 번호
 
+// Express에서 req.body에 접근하여, body 데이터를 사용할 수 있도록 설정하는 미들웨어
+app.use(express.json());
+//일반적인 형태
+// json형태로 서버에 body데이터를 전달 하면,req.body에 데이터를 변환하여 넣어준다.
+app.use(express.urlencoded({ extended: true }));
+// from content type에서 body 데이터를 전달하면, req.body에 데이터를 변환하여 넣어준다.
+// 프론트엔드분들과 협업을 하게 된다면 이런 방식으로 사용해야함
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
--- SPA-SHOP/routes/goods.js
+++ SPA-SHOP/routes/goods.js
@@ -2,21 +2,71 @@
 
 import express from 'express';
 
-// Express.js의 라우터를 생성합니다.
+
 const router = express.Router();
 
 
+// /routes/goods.js
 
-// routes/goods.js
+const goods = [
+  {
+    goodsId: 1,
+    name: '상품 1',
+    thumbnailUrl:
+      'https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg',
+    category: 'drink',
+    price: 6.2,
+  },
+  {
+    goodsId: 2,
+    name: '상품 2',
+    thumbnailUrl:
+      'https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg',
+    category: 'drink',
+    price: 0.11,
+  },
+  {
+    goodsId: 3,
+    name: '상품 3',
+    thumbnailUrl:
+      'https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg',
+    category: 'drink',
+    price: 2.2,
+  },
+  {
+    goodsId: 4,
+    name: '상품 4',
+    thumbnailUrl:
+      'https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg',
+    category: 'drink',
+    price: 0.1,
+  },
+];
 
-// localhost:3000/api/ GET
-router.get('/', (req, res) => {
-    res.json('default url for goods.js GET Method');
-  });
-  
-  // localhost:3000/api/about GET
-  router.get('/about', (req, res) => {
-    res.json('goods.js about PATH');
-  });
+// 상품 목록 조회 api// 
+// http://localhost:3000/api/goods
+// 조회 메서드로 get을 사용
+router.get('/goods', (req, res) => {
+  return res.status(200).json({
+    goods: goods,
+  })
+});
+
+// /routes/goods.js
+
+/** 상품 상세 조회 **/
+// localhost:3000/api/goods/:goodsId
+router.get('/goods/:goodsId', (req, res) => {
+  // 1. 상품의 id조회 하고
+  // 2. 상품 id와 일치 하는 데이터를 찾고
+  // 3. 조회된 상품 정보를 return한다.
+
+// 경로 매개변수 = req.params로 가지고 온다.
+  const goodsId = req.params.goodsId;
+  const findgoods = goods.find((onegoods) => onegoods.goodsId === +goodsId);
+
+  return res.status(200).json({goods: findgoods});
+});
+
 
   export default router;
