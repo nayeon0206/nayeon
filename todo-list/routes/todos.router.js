71dbf44a45505d887416bfa50b45b056981a4618
강나연 <skdusdl4@naver.com>
2024-11-23 14:40:19 +0900

    Update todos.router.js

--- todo-list/routes/todos.router.js
+++ todo-list/routes/todos.router.js
@@ -30,16 +30,47 @@ router.post('/todos', async (req, res, next) => {
   // 생성한 '해야할 일'을 MongoDB에 저장합니다.
   return res.status(201).json({ todo:todo });
 });
-// routes/todos.router.js
 
-// 해야할 일 목록 조회
-router.get('/todos', async (req, res) => {
+// 해야할 일 목록 조회 api
+router.get('/todos', async (req, res, next) => {
     // Todo모델을 이용해, MongoDB에서 'order' 값이 가장 높은 '해야할 일'을 찾습니다.
     const todos = await Todo.find().sort('-order').exec();
   
     // 찾은 '해야할 일'을 클라이언트에게 전달합니다.
     return res.status(200).json({ todos });
   });
+
+// 해야 할 일 순서 변경 api
+
+router.patch('/todos/:todoId', async (req, res, next) => {
+  // 변경할 '해야할 일'의 ID 값을 가져옵니다.
+  const { todoId } = req.params;
+  // '해야할 일'을 몇번째 순서로 설정할 지 order 값을 가져옵니다.
+  const { order } = req.body;
+
+  // 변경하려는 '해야할 일'을 가져옵니다. 만약, 해당 ID값을 가진 '해야할 일'이 없다면 에러를 발생시킵니다.
+  const currentTodo = await Todo.findById(todoId).exec();
+  if (!currentTodo) {
+    return res.status(404).json({ errorMessage: '존재하지 않는 해야할 일 입니다.' });
+  }
+
+  if (order) {
+    // 변경하려는 order 값을 가지고 있는 '해야할 일'을 찾습니다.
+    const targetTodo = await Todo.findOne({ order }).exec();
+    if (targetTodo) {
+      // 만약, 이미 해당 order 값을 가진 '해야할 일'이 있다면, 해당 '해야할 일'의 order 값을 변경하고 저장합니다.
+      targetTodo.order = currentTodo.order;
+      await targetTodo.save();
+    }
+    // 변경하려는 '해야할 일'의 order 값을 변경합니니다.
+    currentTodo.order = order;
+  }
+
+  // 변경된 '해야할 일'을 저장합니다.
+  await currentTodo.save();
+
+  return res.status(200).json({});
+});
 
 export default router;
 
