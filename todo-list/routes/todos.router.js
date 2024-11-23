706ee809d28173e01bb30e32930d10b61931e1ae
강나연 <skdusdl4@naver.com>
2024-11-23 15:39:06 +0900

    Update todos.router.js

--- todo-list/routes/todos.router.js
+++ todo-list/routes/todos.router.js
@@ -6,7 +6,7 @@ import Todo from '../schemas/todo.schema.js';
 const router = express.Router();
 
 // 할일 등록 api// 
-router.post('/todos', async (req, res, next) => {
+router.post('/todos', async (req, res) => {
   // 클라이언트에게 전달받은 value 데이터를 변수에 저장합니다.
   const { value } = req.body;
 
@@ -32,7 +32,7 @@ router.post('/todos', async (req, res, next) => {
 });
 
 // 해야할 일 목록 조회 api
-router.get('/todos', async (req, res, next) => {
+router.get('/todos', async (req, res) => {
     // Todo모델을 이용해, MongoDB에서 'order' 값이 가장 높은 '해야할 일'을 찾습니다.
     const todos = await Todo.find().sort('-order').exec();
   
@@ -40,18 +40,26 @@ router.get('/todos', async (req, res, next) => {
     return res.status(200).json({ todos });
   });
 
-// 해야 할 일 순서 변경 api
+// 해야 할 일 순서 변경 , 완료/ 해제api
 
-router.patch('/todos/:todoId', async (req, res, next) => {
+// /routes/todos.router.js
+
+// /routes/todos.router.js
+
+
+/** 순서 변경, 할 일 완료/해제, 할 일 내용 변경 **/
+router.patch('/todos/:todoId', async (req, res) => {
   // 변경할 '해야할 일'의 ID 값을 가져옵니다.
   const { todoId } = req.params;
-  // '해야할 일'을 몇번째 순서로 설정할 지 order 값을 가져옵니다.
-  const { order } = req.body;
+  // 클라이언트가 전달한 순서, 완료 여부, 내용 데이터를 가져옵니다.
+  const { order, done, value } = req.body;
 
   // 변경하려는 '해야할 일'을 가져옵니다. 만약, 해당 ID값을 가진 '해야할 일'이 없다면 에러를 발생시킵니다.
   const currentTodo = await Todo.findById(todoId).exec();
   if (!currentTodo) {
-    return res.status(404).json({ errorMessage: '존재하지 않는 해야할 일 입니다.' });
+    return res
+      .status(404)
+      .json({ errorMessage: '존재하지 않는 todo 데이터입니다.' });
   }
 
   if (order) {
@@ -65,12 +73,38 @@ router.patch('/todos/:todoId', async (req, res, next) => {
     // 변경하려는 '해야할 일'의 order 값을 변경합니니다.
     currentTodo.order = order;
   }
+  if (done !== undefined) {
+    // 변경하려는 '해야할 일'의 doneAt 값을 변경합니다.
+    currentTodo.doneAt = done ? new Date() : null;
+  }
+  if (value) {
+    // 변경하려는 '해야할 일'의 내용을 변경합니다.
+    currentTodo.value = value;
+  }
 
   // 변경된 '해야할 일'을 저장합니다.
   await currentTodo.save();
 
   return res.status(200).json({});
 });
+
+/** 할 일 삭제 **/
+router.delete('/todos/:todoId', async (req, res) => {
+  // 삭제할 '해야할 일'의 ID 값을 가져옵니다.
+  const { todoId } = req.params;
+
+  // 삭제하려는 '해야할 일'을 가져옵니다. 만약, 해당 ID값을 가진 '해야할 일'이 없다면 에러를 발생시킵니다.
+  const todo = await Todo.findById(todoId).exec();
+  if (!todo) {
+    return res.status(404).json({ errorMessage: '존재하지 않는 해야할 일 정보입니다.' });
+  }
+
+  // 조회된 '해야할 일'을 삭제합니다.
+  await Todo.deleteOne({ _id: todoId }).exec();
+
+  return res.status(200).json({});
+});
+
 
 export default router;
 
