40976755091eb4914aa2a10a25aae4bc9cd8692b
강나연 <skdusdl4@naver.com>
2024-11-27 02:25:38 +0900

    사용자 히스토리 모델까지 구현
    사용자 히스토리 모델까지 구현하기

---
+++ community-hub/src/routes/post.router.js
@@ -0,0 +1,67 @@
+// 1. 게시글을 작성하려는 클라이언트가 로그인된 사용자인지 검증합니다.
+// 2. 게시글 생성을 위한 `title`, `content`를 **body**로 전달받습니다.
+// 3. **Posts** 테이블에 게시글을 생성합니다.
+
+import express from 'express';
+import { prisma } from '../utiles/prisma/index.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+/** 게시글 생성 API **/
+router.post('/posts', authMiddleware, async (req, res, next) => {
+  const { userId } = req.user;
+  const { title, content } = req.body;
+
+  const post = await prisma.posts.create({
+    data: {
+      userId: +userId,
+      title,
+      content,
+    },
+  });
+
+  return res.status(201).json({ data: post });
+});
+
+
+/** 게시글 목록 조회 API **/
+router.get('/posts', async (req, res, next) => {
+    const posts = await prisma.posts.findMany({
+      select: {
+        postId: true,
+        userId: true,
+        title: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+      orderBy: {
+        createdAt: 'desc', // 게시글을 최신순으로 정렬합니다.
+      },
+    });
+  
+    return res.status(200).json({ data: posts });
+  });
+
+/** 게시글 상세 조회 API **/
+router.get('/posts/:postId', async (req, res, next) => {
+    const { postId } = req.params;
+    const post = await prisma.posts.findFirst({
+      where: {
+        postId: +postId,
+      },
+      select: {
+        postId: true,
+        userId: true,
+        title: true,
+        content: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+  
+    return res.status(200).json({ data: post });
+  });
+
+
+export default router;
--- community-hub/src/routes/user.router.js
+++ community-hub/src/routes/user.router.js
@@ -5,46 +5,65 @@ import { prisma } from '../utiles/prisma/index.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import authMiddleware from '../middlewares/auth.middleware.js';
+import { Prisma } from '@prisma/client';
 
 const router = express.Router();
 
-/** 사용자 회원가입 API **/
+/** 사용자 회원가입 API 트랜잭션 **/
 router.post('/sign-up', async (req, res, next) => {
-  const { email, password, name, age, gender, profileImage } = req.body;
-  const isExistUser = await prisma.users.findFirst({
-    where: {
-      email,
-    },
-  });
+  try {
+	  const { email, password, name, age, gender, profileImage } = req.body;
+	  const isExistUser = await prisma.users.findFirst({
+	    where: {
+	      email,
+	    },
+	  });
+	
+	  if (isExistUser) {
+	    return res.status(409).json({ message: '이미 존재하는 이메일입니다.' });
+	  }
+	
+	  // 사용자 비밀번호를 암호화합니다.
+	  const hashedPassword = await bcrypt.hash(password, 10);
 
-  if (isExistUser) {
-    return res.status(409).json({ message: '이미 존재하는 이메일입니다.' });
-  }
 
-  
-    // 사용자 비밀번호를 암호화합니다.
-    const hashedPassword = await bcrypt.hash(password, 10);
-    // Users 테이블에 사용자를 추가합니다.
-    const user = await prisma.users.create({
-      data: {
-        email,
-        password: hashedPassword, // 암호화된 비밀번호를 저장합니다.
+    // MySQL과 연결된 Prisma 클라이언트를 통해 트랜잭션을 실행합니다.
+    const [user, userInfo] = await prisma.$transaction(
+      async (tx) => {
+        // 트랜잭션 내부에서 사용자를 생성합니다.
+        const user = await tx.users.create({
+          data: {
+            email,
+            password: hashedPassword, // 암호화된 비밀번호를 저장합니다.
+          },
+        });
+
+        // throw new Error("고의로 발생시킨 트랜젝션 에러")
+
+        // 트랜잭션 내부에서 사용자 정보를 생성합니다.
+        const userInfo = await tx.userInfos.create({
+          data: {
+            userId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
+            name,
+            age,
+            gender: gender.toUpperCase(), // 성별을 대문자로 변환합니다.
+            profileImage,
+          },
+        });
+
+        // 콜백 함수의 리턴값으로 사용자와 사용자 정보를 반환합니다.
+        return [user, userInfo];
       },
-    });
-  
-    // UserInfos 테이블에 사용자 정보를 추가합니다.
-    const userInfo = await prisma.userInfos.create({
-      data: {
-        userId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
-        name,
-        age,
-        gender: gender.toUpperCase(), // 성별을 대문자로 변환합니다.
-        profileImage,
+      {
+        isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
       },
-    });
-  
+    );
     return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
-  });
+  } catch (err) {
+		next(err);
+  }
+});
+
 
 //   1. `email`, `password`를 **body**로 전달받습니다.
 // 2. 전달 받은 `email`에 해당하는 사용자가 있는지 확인합니다.
