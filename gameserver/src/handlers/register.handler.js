4069e66fcfba2c333e53d8e4d0cb11749833042e
강나연 <skdusdl4@naver.com>
2024-12-13 15:39:39 +0900

    [add]

--- gameserver/src/handlers/register.handler.js
+++ gameserver/src/handlers/register.handler.js
@@ -1,5 +1,6 @@
 import { addUser } from '../models/user.model.js';
 import { v4 as uuidv4 } from 'uuid';
+import { handleConnection, handleDisconnect } from './helper.js';
 
 const registerHandler = (io) => {
     io.on('connection', (socket) => {
@@ -8,6 +9,10 @@ const registerHandler = (io) => {
       
     const userUUID = uuidv4(); // UUID 생성
     addUser({ uuid: userUUID, socketId: socket.id }); // 사용자 추가
+    createStage(userUUID);
+
+    // 접속시 유저 정보 생성 이벤트 처리
+    handleConnection(socket, userUUID);
 
     // 접속 해제시 이벤트 처리
     socket.on('disconnect', () => handleDisconnect(socket, userUUID));
